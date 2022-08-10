62056bb0cdb91b50423972aa43566d9b5cbbcf0e
Toshinori Sugita <s.toshi0607@gmail.com>
2022-08-10 20:40:42 +0900

    Validate state

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -48,10 +48,12 @@ app.get('/authorize', function(req, res){
 	/*
 	 * Send the user to the authorization server
 	 */
+	state = randomstring.generate()
 	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
 		response_type: 'code',
 		client_id: client.client_id,
-		redirect_uri: client.redirect_uris[0]
+		redirect_uri: client.redirect_uris[0],
+		state: state
 	});
 	res.redirect(authorizeUrl);
 });
@@ -61,6 +63,11 @@ app.get('/callback', function(req, res){
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
+	if (req.query.state != state) {
+		res.render('error', { error: 'State value did not match' });
+		return;
+	}
+
 	var code = req.query.code
 	var form_data = qs.stringify({
 		grant_type: 'authorization_code',
